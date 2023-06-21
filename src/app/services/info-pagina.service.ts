ed1a67bebfb120d7ce02e8565bc12a53171b6e58
Gerardo Cornalo <gerardocornalo@gmail.com>
2023-06-21 10:14:03 -0300

    datos tomados de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,42 @@ export class InfoPaginaService {
   info: InfoPagina ={};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) { 
     //console.log('servicio de infopagina listo');
 
-    // leer el archivo Json
-
-    this.http.get('/assets/data/data-pagina.json')
-              .subscribe( (resp: InfoPagina)  => {
-                this.cargada = true;
-                this.info = resp;
-                console.log(resp);
+   this.cargarInfo();
+   this.cargarEquipo();
 
 
-  });
+}
+private cargarInfo() {
+   // leer el archivo Json
+
+   this.http.get('/assets/data/data-pagina.json')
+   .subscribe( (resp: InfoPagina)  => {
+     this.cargada = true;
+     this.info = resp;
+    
+
+
+});
+
+
+
+}
+
+private cargarEquipo() {
+
+// leer el archivo Json
+
+this.http.get('https://angular-herrera-default-rtdb.firebaseio.com/equipo.json')
+.subscribe( (resp: any)  => {
+ 
+  this.equipo = resp;
+  console.log(resp);
+
+});
 }
 }
